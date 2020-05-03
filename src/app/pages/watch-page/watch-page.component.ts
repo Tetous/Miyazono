6047f9fc270839ca483a130423ebe31b6692a426
Fmaldonado6 <f_maldonado6@hotmail.com>
2020-05-03 03:08:18 -0500

    Watch page episode fix 2

--- src/app/pages/watch-page/watch-page.component.ts
+++ src/app/pages/watch-page/watch-page.component.ts
@@ -1,7 +1,7 @@
 import { AnimeService } from './../../services/anime.service';
 import { AnimeData } from './../../services/animeData.service';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router,NavigationExtras } from '@angular/router';
+import { ActivatedRoute, Router, NavigationExtras } from '@angular/router';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 @Component({
   selector: 'app-watch-page',
@@ -11,13 +11,13 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 export class WatchPage implements OnInit {
 
   episode = {
-    animeId:"",
+    animeId: "",
     title: "",
     number: "",
-    otherEpisodes:[],
+    otherEpisodes: [],
     servers: []
   }
- 
+
   states = {
     loading: 0,
     loaded: 1,
@@ -29,37 +29,39 @@ export class WatchPage implements OnInit {
   url: SafeResourceUrl = this.sanitizer.bypassSecurityTrustResourceUrl("");
 
 
-  constructor(private animeData: AnimeData, private animeService: AnimeService, private route: ActivatedRoute, private sanitizer: DomSanitizer, private router:Router) {
+  constructor(private animeData: AnimeData, private animeService: AnimeService, private route: ActivatedRoute, private sanitizer: DomSanitizer, private router: Router) {
     console.log("changing")
-    if (animeData.episodeData == undefined) {
-      route.queryParams.subscribe(params => {
+    route.queryParams.subscribe(params => {
+      if (animeData.episodeData == undefined) {
         let id = params.episodeId
         animeService.getServers(id).subscribe(servers => {
           this.episode.servers = servers["servers"];
           this.episode.title = params.episodeTitle
           this.episode.number = params.episodeNumber
-          this.currentEpisode=this.episode.number
+          this.currentEpisode = this.episode.number
           this.episode.animeId = params.animeId
           this.getEpisodes();
           this.changeUrl(this.episode.servers[0].code)
-
-
         })
-      })
 
-    } else {
-      this.episode = animeData.episodeData
-      this.currentEpisode=this.episode.number
-      this.getEpisodes();
-      this.changeUrl(this.episode.servers[0].code)
-    }
+      } else {
+        this.episode = animeData.episodeData
+        this.currentEpisode = this.episode.number
+        this.getEpisodes();
+        this.changeUrl(this.episode.servers[0].code)
+      }
 
+    })
 
   }
 
-  getEpisodes(){
+  getEpisodes() {
     this.currentState = this.states.loading
+    console.log("getting results")
+
     this.animeService.getSearchResult(this.episode.title).subscribe(results => {
+      console.log(results)
+
       let current = 0
       while (results["search"][current].title != this.episode.title) {
         current++;
@@ -73,32 +75,31 @@ export class WatchPage implements OnInit {
     })
   }
 
-  changeUrl(url){
+  changeUrl(url) {
     this.url = this.sanitizer.bypassSecurityTrustResourceUrl(url);
-    
+
   }
   changeEpisodePage(episode) {
-    console.log(episode)
     this.currentState = this.states.loading
 
-    this.currentEpisode=episode.episode
-   
-      let extras: NavigationExtras = {
-        queryParams: {
-          "episodeId": episode.id,
-          "animeId":this.episode.animeId,
-          "episodeTitle": this.episode.title,
-          "episodeNumber": episode.episode,
+    this.currentEpisode = episode.episode
 
-        }
+    let extras: NavigationExtras = {
+      queryParams: {
+        "episodeId": episode.id,
+        "animeId": this.episode.animeId,
+        "episodeTitle": this.episode.title,
+        "episodeNumber": episode.episode,
+
       }
-      this.router.navigate(["/watch"], extras)
+    }
+    this.router.navigate(["/watch"], extras)
 
   }
-  scroll(element:HTMLElement,event){
+  scroll(element: HTMLElement, event) {
     let scrollValue = element.scrollLeft
-    let scrollAmount= event.deltaY <0 ? scrollValue+150: scrollValue-150
-    element.scroll({left:scrollAmount})
+    let scrollAmount = event.deltaY < 0 ? scrollValue + 150 : scrollValue - 150
+    element.scroll({ left: scrollAmount })
   }
   ngOnInit(): void {
 
